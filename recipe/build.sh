b25960912741c8e3290ed0e4d56cf5248a6b77e9
Travis Hathaway <travis.j.hathaway@gmail.com>
2026-03-26 15:37:52 +0100

    another try at getting cross compilation to work

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,16 +5,5 @@ set -ex
 # which will be provided as conda dependencies
 rm -rf contrib
 
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
-    # Target an Apple Silicon build from an Intel macOS builder.
-    export ARCHFLAGS="-arch arm64"
-    export MACOSX_DEPLOYMENT_TARGET="${MACOSX_DEPLOYMENT_TARGET:-11.0}"
-
-    # conda-build sets these; pass the sysroot through to CMake explicitly.
-    export CMAKE_ARGS="${CMAKE_ARGS:-} \
-      -DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT} \
-      -DCMAKE_OSX_ARCHITECTURES=arm64"
-fi
-
 # Build and install the package
 ${PYTHON} -m pip install . -vv --no-deps --no-build-isolation
