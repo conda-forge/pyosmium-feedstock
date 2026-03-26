2a3fef6d8a014097ebad96e754f9030b101716b0
Travis Hathaway <travis.j.hathaway@gmail.com>
2026-03-26 14:27:53 +0100

    updating build script to be cross-compile compatible

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,5 +5,16 @@ set -ex
 # which will be provided as conda dependencies
 rm -rf contrib
 
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
+
+    # Target an Apple Silicon build from an Intel macOS builder.
+    export ARCHFLAGS="-arch arm64"
+
+    # conda-build sets these; pass the sysroot through to CMake explicitly.
+    export CMAKE_ARGS="${CMAKE_ARGS:-} \
+      -DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT} \
+      -DCMAKE_OSX_ARCHITECTURES=arm64"
+fi
+
 # Build and install the package
 ${PYTHON} -m pip install . -vv --no-deps --no-build-isolation
